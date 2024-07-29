dec115d734b3c3fd4791409622a3d6685bce3e1b
sgb-2699 <167523788+sgb-2699@users.noreply.github.com>
2024-07-29 22:53:34 +0530

    Update .merge.c

--- .merge.c
+++ .merge.c
@@ -1,33 +1,63 @@
 #include <stdio.h>
-#include <time.h>
 
-void printArray(int a[], int size) {
-    for(int i = 0; i < size; i++){
-        printf("%d ", a[i]);
+int size = 6;
+
+void merge(int a[], int low, int mid, int high) {
+    int i = low;
+    int j = mid + 1;
+    int k = low;
+
+    int b[6];
+
+    while(i <= mid && j <= high) {
+        if(a[i] <= a[j]) {
+            b[k] = a[i];
+            i++;
+        } else {
+            b[k] = a[j];
+            j++;
+        }
+        k++;
     }
+
+    if(i > mid) {
+        while(j <= high) {
+            b[k] = a[j];
+            j++;
+            k++;
+        }
+    } else {
+        while(i <= mid) {
+            b[k] = a[i];
+            i++;
+            k++;
+        }
+    }
+
+    for(i = low; i <= high; i++) {
+        a[i] = b[i];
+    }
+}
+
+void mergeSort(int a[], int low, int high) {
+    if(low != high) {
+        int mid = (low+high)/2;
+        mergeSort(a,low,mid);
+        mergeSort(a,mid+1,high);
+        merge(a,low,mid,high);
+    }
+}
+
+void printArray(int arr[], int size) {
+    
+    for (int i = 0; i < size; i++)
+        printf("%d ", arr[i]);
     printf("\n");
 }
 
-void insertionSort(int a[], int size) {
-    for(int i = 1; i < size; i++) {
-        int temp = a[i];
-        int j = i - 1;
-        while(j>=0 && a[j]>temp) {
-            a[j+1] = a[j];
-            j--;
-        }
-        a[j+1] = temp;
-    }
-    printArray(a, size);
-}
-
 int main() {
-    int start, stop;
-    int arr[] = {5, 4, 10, 1, 6, 2};
-    start = clock();
-    insertionSort(arr, 6);
-    stop = clock();
-    printf("Time taken to complete insertion sort is %lf seconds.",(double)((stop-start)/CLOCKS_PER_SEC));
-
+    int arr[] = {1,6,5,4,2,3};
+    mergeSort(arr,0,size - 1);
+    printArray(arr, 6);
     return 0;
 }
